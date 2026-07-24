4369322050b4ebf68627f68e84f0da98d0f140be
Max Krasnyansky <maxk@qti.qualcomm.com>
2026-07-24 14:14:44 -0700

    hex-bufs: use same get_alignment for host bufs

--- ggml/src/ggml-hexagon/ggml-hexagon.cpp
+++ ggml/src/ggml-hexagon/ggml-hexagon.cpp
@@ -1168,9 +1168,9 @@ static ggml_backend_buffer_type_i ggml_backend_hexagon_buffer_type_interface = {
 static ggml_backend_buffer_type_i ggml_backend_hexagon_host_buffer_type_interface = {
     /* .get_name         = */ ggml_backend_hexagon_buffer_type_name,
     /* .alloc_buffer     = */ ggml_backend_hexagon_host_buffer_type_alloc_buffer,
-    /* .get_alignment    = */ ggml_backend_cpu_buffer_type()->iface.get_alignment,
-    /* .get_max_size     = */ NULL,
-    /* .get_alloc_size   = */ ggml_backend_cpu_buffer_type()->iface.get_alloc_size,
+    /* .get_alignment    = */ ggml_backend_hexagon_buffer_type_get_alignment,
+    /* .get_max_size     = */ ggml_backend_hexagon_buffer_type_get_max_size,
+    /* .get_alloc_size   = */ ggml_backend_hexagon_buffer_type_get_alloc_size,
     /* .is_host          = */ ggml_backend_hexagon_host_buffer_type_is_host,
 };
 
