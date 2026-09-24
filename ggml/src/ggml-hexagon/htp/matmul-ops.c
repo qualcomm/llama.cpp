47432c5f1c21bc4bda75f96d367ddf79b81d01df
Max Krasnyansky <maxk@qti.qualcomm.com>
2026-09-24 11:33:03 -0700

    Update ggml/src/ggml-hexagon/htp/matmul-ops.c
    Co-authored-by: Sigbjørn Skjæret <sigbjorn.skjaeret@huggingface.co>

--- ggml/src/ggml-hexagon/htp/matmul-ops.c
+++ ggml/src/ggml-hexagon/htp/matmul-ops.c
@@ -4040,8 +4040,8 @@ static int hvx_mm_matmul_id_nx(
     octx->src3_spad.src = NULL;
     octx->dst_spad.src  = NULL;
 
-    mmctx->vtcm_src0_stride     = 0;
-    mmctx->vtcm_src1_stride     = src1_row_size;
+    mmctx->vtcm_src0_stride    = 0;
+    mmctx->vtcm_src1_stride    = src1_row_size;
     mmctx->vtcm_act_raw_stride = hex_round_up(act->ne[0] * sizeof(float), QK_Q8_0_TILED * sizeof(float));
 
     mmctx->vtcm_src0_size_per_thread = fastdiv(L.src0_bytes, &octx->n_threads_div);
